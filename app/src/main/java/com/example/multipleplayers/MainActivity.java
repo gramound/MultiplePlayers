880b08b46271eb48d001c75a7aef19c274256d7a
Dominic Lemire <dominic.lemire@gmail.com>
2025-02-03 13:39:06 -0800

    Add option to compare a shared Allocator vs individual

--- app/src/main/java/com/example/multipleplayers/MainActivity.java
+++ app/src/main/java/com/example/multipleplayers/MainActivity.java
@@ -22,11 +22,16 @@ import androidx.media3.exoplayer.trackselection.DefaultTrackSelector;
 import androidx.media3.exoplayer.upstream.DefaultAllocator;
 import androidx.media3.ui.PlayerView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 @OptIn(markerClass = UnstableApi.class)
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "MultiplePlayers";
     private GridLayout mLayout;
-    private DefaultAllocator mAllocator;
+    private final boolean mShareAllocator = true;
+    private final List<DefaultAllocator> mAllocators = new ArrayList<>();
     private final Uri VIDEO_URI = Uri.parse("https://storage.googleapis.com/wvmedia/clear/hevc/30fps/llama/llama_hevc_480p_30fps_3000.mp4");
     private final long VIDEO_DURATION_MS = 90_000;
     private final int NUM_PLAYERS = 9;
@@ -54,7 +59,11 @@ public class MainActivity extends AppCompatActivity {
         String title = NUM_PLAYERS + " players using " + MediaLibraryInfo.VERSION_SLASHY;
         setTitle(title);
         Log.d(TAG, title);
-        mAllocator = new DefaultAllocator(false, C.DEFAULT_BUFFER_SEGMENT_SIZE);
+        if (mShareAllocator) {
+            mAllocators.add(new DefaultAllocator(false, C.DEFAULT_BUFFER_SEGMENT_SIZE));
+        } else for (int i = 0; i < NUM_PLAYERS; i++) {
+            mAllocators.add(new DefaultAllocator(true, C.DEFAULT_BUFFER_SEGMENT_SIZE));
+        }
         mHandler = new Handler(getMainLooper());
     }
 
@@ -67,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
             ExoPlayer player = new ExoPlayer.Builder(this)
                     // Share allocator to avoid multiple AVAILABLE_EXTRA_CAPACITY
                     .setLoadControl(new DefaultLoadControl.Builder()
-                            .setAllocator(mAllocator)
+                            .setAllocator(mAllocators.get(mShareAllocator ? 0 : i))
                             .build())
                     // Split the bandwidth
                     .setTrackSelector(new DefaultTrackSelector(this, new AdaptiveTrackSelection.Factory(
@@ -81,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
             player.setRepeatMode(Player.REPEAT_MODE_ONE);
             player.seekTo(i * VIDEO_DURATION_MS / (NUM_PLAYERS+1));
             player.prepare();
+            final int finalI = i;
             player.addListener(new Player.Listener() {
                 @Override
                 public void onPlaybackStateChanged(int playbackState) {
@@ -93,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                         case Player.STATE_READY: playbackStateString = "ready"; break;
                         default: playbackStateString = "unknown"; break;
                     }
-                    logTotalBytesAllocated(String.format("playbackState:%s", playbackStateString));
+                    logTotalBytesAllocated(String.format(Locale.US, "Player%d:%s", finalI, playbackStateString));
                 }
             });
             playerView.setPlayer(player);
@@ -129,12 +139,21 @@ public class MainActivity extends AppCompatActivity {
             stopPlayer(i);
         }
         logTotalBytesAllocated("before trim");
-        mAllocator.setTargetBufferSize(0);
+        if (mShareAllocator) {
+            mAllocators.get(0).setTargetBufferSize(0);
+        } else for (int i = 0; i < NUM_PLAYERS; i++) {
+            mAllocators.get(i).setTargetBufferSize(0);
+        }
         logTotalBytesAllocated("after trim");
     }
 
     private void logTotalBytesAllocated(String prefix) {
-        int totalSize = mAllocator.getTotalBytesAllocated();
+        int totalSize = 0;
+        if (mShareAllocator) {
+            totalSize = mAllocators.get(0).getTotalBytesAllocated();
+        } else for (int i = 0; i < NUM_PLAYERS; i++) {
+            totalSize += mAllocators.get(i).getTotalBytesAllocated();
+        }
         Log.d(TAG, prefix + ": " + Formatter.formatShortFileSize(this, totalSize));
     }
 }
