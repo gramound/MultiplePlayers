06d453a3455dd002a2d46a4d9a33a09598c4cac4
Dominic Lemire <dominic.lemire@broadcom.com>
2025-06-04 17:14:50 -0700

    Bare minimum for 9x720p cenc

--- app/src/main/java/com/example/multipleplayers/MainActivity.java
+++ app/src/main/java/com/example/multipleplayers/MainActivity.java
@@ -4,14 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.GridLayout;
 
+import androidx.annotation.OptIn;
+import androidx.media3.common.C;
 import androidx.media3.common.MediaItem;
 import androidx.media3.common.Player;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.common.util.Util;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.ui.PlayerView;
 
+@OptIn(markerClass = UnstableApi.class)
 public class MainActivity extends Activity {
     private static final int NUM_PLAYERS = 9;
-    private static final String URI = "https://storage.googleapis.com/wvmedia/clear/hevc/60fps/llama/llama_hevc_720p_60fps_4000.mp4";
+    private static final String URI = "https://storage.googleapis.com/wvmedia/cenc/hevc/60fps/llama/llama_hevc_720p_60fps_4000.mp4";
+    private static final String L1_LICENSE_URI = "https://proxy.uat.widevine.com/proxy?video_id=GTS_HW_SECURE_ALL&provider=widevine_test";
+    private static final String L3_LICENSE_URI = "https://proxy.uat.widevine.com/proxy?video_id=GTS_SW_SECURE_CRYPTO&provider=widevine_test";
+    private final String LICENSE_URI = Util.isRunningOnEmulator() ? L3_LICENSE_URI : L1_LICENSE_URI;
     private final PlayerView[] mPlayerViews = new PlayerView[NUM_PLAYERS];
 
     @Override
@@ -40,7 +48,14 @@ public class MainActivity extends Activity {
         super.onStart();
         for (int i = 0; i < NUM_PLAYERS; i++) {
             ExoPlayer player = new ExoPlayer.Builder(this).build();
-            player.setMediaItem(MediaItem.fromUri(URI));
+            player.setMediaItem(new MediaItem.Builder()
+                    .setUri(URI)
+                    .setDrmConfiguration(
+                            new MediaItem.DrmConfiguration.Builder(C.WIDEVINE_UUID)
+                                    .setLicenseUri(LICENSE_URI)
+                                    .build()
+                    )
+                    .build());
             player.setPlayWhenReady(true);
             player.setRepeatMode(Player.REPEAT_MODE_ONE);
             player.prepare();
